6f029b929a1473ea6d267865eaff119a56b90d6c
vinayydv3695 <164752534+vinayydv3695@users.noreply.github.com>
2024-04-12 08:25:35 +0530

    Update arrayss.java

--- arrayss.java
+++ arrayss.java
@@ -2,17 +2,17 @@ public class arrayss {
     public static void main(String[] args) {
 
 
-                // Declare and initialize an integer array of size 5
+            
                 int[] numbers = new int[5];
 
-                // Assign values to the array elements
+        
                 numbers[0] = 10;
                 numbers[1] = 20;
                 numbers[2] = 30;
                 numbers[3] = 40;
                 numbers[4] = 50;
 
-                // Access and print array elements
+            
                 for (int i = 0; i < numbers.length; i++) {
                     System.out.println("Element at index " + i + ": " + numbers[i]);
                 }
