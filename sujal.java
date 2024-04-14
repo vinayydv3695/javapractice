89e29d582d94e5d7b0639f753e99519faf476f9e
vinayydv3695 <vinaydv343@gmail.com>
2024-04-14 18:55:00 +0530

    java from scaratch

---
+++ sujal.java
@@ -0,0 +1,40 @@
+
+
+import java.util.Scanner;
+public class sujal {
+    public static void main(String[] args) {
+
+        Scanner noob = new Scanner(System.in);
+        String name = "";
+        String day = noob.nextLine();
+        switch (day) {
+            case "sunday":
+                System.out.println("it is sunday");
+                break;
+            case "sundcbay":
+                System.out.println("it is sunday");
+                break;
+            case "sunray":
+                System.out.println("it is sunray");
+                break;
+            case "sundfay":
+                System.out.println("it is sunfay");
+                break;
+            case "sunddfab":
+                System.out.println("it is sundfab");
+                break;
+            case "sunddy":
+                System.out.println("it is sunday");
+                break;
+            case "sundy":
+                System.out.println("it is sunday");
+                break;
+            case "sundfvay":
+                System.out.println("it is sunday");
+                break;
+            default :
+               System.out.println ("not valid bitch check your spelling");
+
+        }
+    }
+}
